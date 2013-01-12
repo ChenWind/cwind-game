015130f04a1ccf5cb5f3440900818ef27c83f7aa
ChenWind <Chentian200@gmail.com>
2013-01-12 15:12:08 +0800

    ce

--- src/com/cwind/net/common/entity/NetTpCommand.java
+++ src/com/cwind/net/common/entity/NetTpCommand.java
@@ -16,11 +16,31 @@ public class NetTpCommand {
 	/***
 	 * ÍøÂçÁ¬½Ó
 	 */
-	public static final String NetConnect = "001";
-	public static final String NetTestPack = "002";
-	public static final String NetLogin = "003";
-	public static final String NetLogout = "004";
-	public static final String GetData = "005";
-	public static final String UploadFile = "006";
-	public static final String DownloadFile = "007";
+	public static final byte InvalidCommand = 0;
+	public static final byte NetConnect = 1;
+	public static final byte NetTestPack = 2;
+	public static final byte NetLogin = 3;
+	public static final byte NetLogout = 4;
+	public static final byte GetData = 5;
+	public static final byte UploadFile = 6;
+	public static final byte DownloadFile = 7;
+
+	/**
+	 * 
+	 * @param strCmd
+	 * @return
+	 * @description ½âÎö×Ö·û´®µÄÃüÁî
+	 * @Date 2013 2013-1-12
+	 * @author chenzf
+	 */
+	public byte parseStringCommand(String strCmd) {
+		try {
+			byte cmd = Byte.parseByte(strCmd);
+			if (cmd < InvalidCommand || cmd > DownloadFile)
+				return InvalidCommand;
+			return cmd;
+		} catch (Exception e) {
+			return InvalidCommand;
+		}
+	}
 }
---
+++ src/com/cwind/net/common/entity/NetTpStatusCode.java
@@ -0,0 +1,15 @@
+package com.cwind.net.common.entity;
+
+/**
+ * 
+ * @description 错误码
+ *              <p>
+ *              均以short类型来表示
+ *              </p>
+ * @author chenzf
+ * @Date 2013 2013-1-12
+ */
+public class NetTpStatusCode {
+	public short SendSuccess = 1001;
+	public short RecSuccess = 1002;
+}
